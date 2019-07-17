2c3b31490275d6dd95c893c41b6ec6c3e4848dce
Raul <raulliston@hotmail.com>
2019-07-17 19:42:18 -0300

    Agora ele anda

--- simulator.py
+++ simulator.py
@@ -14,7 +14,21 @@ def move_sequence(car, directions):
   pass
 
 def move(car, direction):
-  pass
+    if direction == 'N':
+        car[0] = car[0] - 1
+    elif direction == 'S':
+        car[0] = car[0] + 1
+    elif direction == 'L':
+        car[1] = car[1] + 1
+    elif direction == 'w':
+        car[1] = car[1] - 1
+    else:
+        print("Invalid direction")
+    return car
+
+move(car, 'K')
+print(car)
+
 
 print(move_sequence(car, "NNLLNNO") == [0, 1])
 
