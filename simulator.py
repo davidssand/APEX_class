be1856add287511e9796b865715da7e82373771a
David <davidssand@hotmail.com>
2019-07-17 19:04:33 -0300

    Initial version

---
+++ simulator.py
@@ -0,0 +1,21 @@
+scenario = [
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1]
+            ]
+
+car = [0, 0]
+
+def boundry_restrictions(coordinates):
+  pass
+
+def move_sequence(car, directions):
+  pass
+
+def move(car, direction):
+  pass
+
+print(move_sequence(car, "SSLLNNNL") == [1, 3])
+
+print("done")
